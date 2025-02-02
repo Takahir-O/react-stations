c03b4b7a15d5254f8fa08781f34604441bd696fc
Takahir-O <orihack3553@gmail.com>
2025-02-02 15:06:50 +0900

    station6クリア時点

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 // DO NOT DELETE
 
+import React, { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg',
+  )
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>Dogアプリ</header>
+      <p>犬の画像を表示するアプリです</p>
+      <img src={dogUrl} alt="犬の画像" />
+      <button
+        onClick={() =>
+          fetch('https://dog.ceo/api/breeds/image/random')
+            .then((res) => res.json())
+            .then((data) => setDogUrl(data.message))
+        }
+      >
+        更新
+      </button>
     </div>
   )
 }
